e0eb887405fe2c38f1814e7bd650726838dd73aa
liyunkuo <79588186+liyunkuo@users.noreply.github.com>
2021-03-03 02:38:30 +0800

    Add files via upload

---
+++ 0226-4.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+作業四
+
+使用for迴圈，列出菱形圖形：
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+"""
+
+for i in range(1,5) :
+    for j in range(1,5) :       # 左上角
+        if (i+j) < 5 :
+            print(" ",end = '')
+        else :
+            print("*",end = '')
+    for k in range(1,4) :       # 右上角
+        if i >= 2 and j >= 4 and (i+k) > 4 :
+            print("*",end = '')
+    print()
+
+times = 2
+
+for i in range(1,5) :
+    for j in range(1,5) :       # 左下角
+        if (i+j) > times :
+            print("*",end = '')
+        else :
+            print(" ",end = '')
+    for k in range(1,3) :       # 右下角
+        if i < 3 and (i+k) >= times :
+            print("*",end = '')
+    print()
+    times += 2
+
+print("程式執行完畢")
